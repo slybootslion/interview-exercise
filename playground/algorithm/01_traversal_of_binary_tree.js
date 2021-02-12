fa691eaf41eae0737750401b8bba86ca2938587e
slybootslion <slybootslion@vip.qq.com>
2021-02-12 15:21:48 +0800

    2021-02-12 15:21:48

---
+++ playground/algorithm/01_traversal_of_binary_tree.js
@@ -0,0 +1,48 @@
+const root = {
+  val: 'A',
+  left: {
+    val: 'B',
+    left: {
+      val: 'D'
+    },
+    right: {
+      val: 'E'
+    }
+  },
+  right: {
+    val: 'C',
+    right: {
+      val: 'F'
+    }
+  }
+}
+
+// 先序遍历
+function preOrder (root) {
+  if (!root) return
+  console.log('当前遍历的节点：', root.val)
+  preOrder(root.left)
+  preOrder(root.right)
+}
+
+// 中序遍历
+function inOrder (root) {
+  if (!root) return
+  inOrder(root.left)
+  console.log('当前遍历的节点：', root.val)
+  inOrder(root.right)
+}
+
+// 后序遍历
+function postOrder (root) {
+  if (!root) return
+  postOrder(root.left)
+  postOrder(root.right)
+  console.log('当前遍历的节点：', root.val)
+}
+
+preOrder(root)
+console.log('-------')
+inOrder(root)
+console.log('-------')
+postOrder(root)
